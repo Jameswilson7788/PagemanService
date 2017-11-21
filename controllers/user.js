94788d6ba250a9a1a60e166d1c147871ffbafe5a
JamesWilson <wesstation2@gmail.com>
2017-11-21 16:04:17 +0800

    d

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,28 @@
 const User = require('../models/user')
 module.exports = {
-  getUser: function (req, res, next) {
-    User.find()
-  }
+  getUserById: function (req, res, next) {
+    User.find({
+      _id: req.params.id
+    }, function (e, user) {
+      if (e) throw e;
+      res.status(200).json(user);
+    })
+  },
+  signup: function (req, res, next) {
+    var body = _.pick(req.body, ['name', 'email', 'password']);
+    User.find({
+      email: body.email
+    }, function (e, email) {
+      if (e) throw e;
+      if (email) return res.status(401).json({
+        msg: '該電子信箱已被註冊'
+      });
+      const user = new User(body);
+      user.save(function (e, user) {
+        if (e) throw e;
+        res.status(200).json('使用者：' + user.email + '  註冊成功！');
+      });
+    });
+  },
+
 }
