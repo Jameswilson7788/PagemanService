6e6f8f4739f792e087a4062e1c8a96b22dcf1eab
JamesWilson <wesstation2@gmail.com>
2017-11-23 12:50:50 +0800

    好好講你不要衝動

--- config/secret.js
+++ config/secret.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db:'mongodb://root:abc123@ds155097.mlab.com:55097/pageman'
+    db:'mongodb://root:abc123@ds155097.mlab.com:55097/pageman',
+    secret: "eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiV2VpZ28iLCJkZXNjcmlwdGlvbiI6ImhhbmRzb21lIn0.1up1NpzqB_COKRSre3-eVM7eU5qrEHH9SWm_14-CGUU"
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const User = require('../models/user');
 const userController = require('../controllers/user')
 /* GET users listing. */
+
 router.post('/signup', userController.signup);
 
 module.exports = router;
